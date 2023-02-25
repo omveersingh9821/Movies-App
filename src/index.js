d6ebe3d6207601fe62b2eaeed677ad1e5130e777
Omveer Singh <omveersingh9821@gmail.com>
2023-02-25 23:54:10 +0530

    dispatch actions

--- src/index.js
+++ src/index.js
@@ -12,6 +12,13 @@ const store = createStore(movies);
 console.log('store', store);
 console.log('before state', store.getState());
 
+store.dispatch({
+  type: 'ADD_MOVIES',
+  movies: [{ name: 'Superman' }]
+});
+
+console.log('after state', store.getState());
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
