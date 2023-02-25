5bf543eac061f11596149493f278438592d178ec
Omveer Singh <omveersingh9821@gmail.com>
2023-02-26 00:29:38 +0530

    Subscribe to the Store

--- src/index.js
+++ src/index.js
@@ -10,20 +10,20 @@ import movies from './reducers';
 
 const store = createStore(movies);
 console.log('store', store);
-console.log('before state', store.getState());
+// console.log('before state', store.getState());
 
-store.dispatch({
-  type: 'ADD_MOVIES',
-  movies: [{ name: 'Superman' }]
-});
+// store.dispatch({
+//   type: 'ADD_MOVIES',
+//   movies: [{ name: 'Superman' }]
+// });
 
-console.log('after state', store.getState());
+// console.log('after state', store.getState());
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App store={store} />
   </React.StrictMode>
 );
 
